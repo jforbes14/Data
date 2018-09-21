fe8cb258f4e25c0f3a77bc8350664a533afc5fb7
jforbes14 <jefor3@student.monash.edu>
2018-09-21 17:01:41 +1000

    Includes centroids

--- Imputation/fortify-maps.R
+++ Imputation/fortify-maps.R
@@ -7,7 +7,7 @@ fortified_2016 <- ggplot2::fortify(simp_abs16) %>%
   mutate(year = "2016",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_abs16@data %>% select(CED_NAME)
+nms <- simp_abs16@data %>% select(CED_NAME, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2016 <- left_join(fortified_2016, nms, by="id") %>% 
   mutate(Election_Division =  toupper(CED_NAME)) %>% 
@@ -18,7 +18,7 @@ fortified_2013 <- ggplot2::fortify(simp_aec13) %>%
   mutate(year = "2013",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_aec13@data %>% select(Elect_div)
+nms <- simp_aec13@data %>% select(Elect_div, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2013 <- left_join(fortified_2013, nms, by="id") %>% 
   mutate(Election_Division =  toupper(Elect_div)) %>% 
@@ -29,7 +29,7 @@ fortified_2010 <- ggplot2::fortify(simp_aec10) %>%
   mutate(year = "2010",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_aec10@data %>% select(ELECT_DIV)
+nms <- simp_aec10@data %>% select(ELECT_DIV, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2010 <- left_join(fortified_2010, nms, by="id") %>% 
   mutate(Election_Division =  toupper(ELECT_DIV)) %>% 
@@ -40,7 +40,7 @@ fortified_2007 <- ggplot2::fortify(simp_abs06e07) %>%
   mutate(year = "2007",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_abs06e07@data %>% select(NAME_2007)
+nms <- simp_abs06e07@data %>% select(NAME_2007, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2007 <- left_join(fortified_2007, nms, by="id") %>% 
   mutate(Election_Division =  toupper(NAME_2007)) %>% 
@@ -51,7 +51,7 @@ fortified_2004 <- ggplot2::fortify(simp_abs06e04) %>%
   mutate(year = "2004",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_abs06e04@data %>% select(NAME_2004)
+nms <- simp_abs06e04@data %>% select(NAME_2004, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2004 <- left_join(fortified_2004, nms, by="id") %>% 
   mutate(Election_Division =  toupper(NAME_2004)) %>% 
@@ -62,11 +62,12 @@ fortified_2001 <- ggplot2::fortify(simp_abs01) %>%
   mutate(year = "2001",
          group = paste0("g.", group),
          piece = paste0("p.", piece))
-nms <- simp_abs01@data %>% select(CED_NAME_2001)
+nms <- simp_abs01@data %>% select(CED_NAME_2001, long_c, lat_c)
 nms$id <- as.character(1:150)
 fortified_2001 <- left_join(fortified_2001, nms, by="id") %>% 
   mutate(Election_Division =  toupper(CED_NAME_2001)) %>% 
   select(-CED_NAME_2001)
+
 
 ## Save
 save(fortified_2001, file = "Clean/Maps/fortified_2001.rda")
